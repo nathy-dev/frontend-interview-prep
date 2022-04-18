45005c62e5a5d60cee30bdf28a25e7424bae6786
nathy <nmackinl@asu.edu>
2022-04-18 15:49:45 -0700

    fixed pokemon app

--- pokedex-vanilla/index.js
+++ pokedex-vanilla/index.js
@@ -3,43 +3,49 @@ const API_URL = 'https://pokeapi.co/api/v2/type/';
 const searchButton = document.getElementById('search-button');
 const pokemonList = document.getElementById('search-results');
 const searchBar = document.getElementById('search-bar');
-const form = document.querySelector('.search-form')
+const form = document.querySelector('.search-form');
+const container = document.querySelector('.wrapper');
 
-searchButton.addEventListener('click', getPokemonType)
+searchButton.addEventListener('click', handleSearchResults)
 
-async function getPokemonType(type='fire') {
+async function getPokemonType(search) {
+   try {
     searchButton.disabled = true;
-    let pokemon;
-    const searchResults = await fetch(`${API_URL}${type}`)
-        .then(res => res.json())
-        .then(res => res.pokemon)
-        .catch(e => console.log('oh no!'))
-        .finally(res => pokemon = res)
-        searchButton.disabled = false;
-        return pokemon;
+    const url = (API_URL + search).toString();
+    const searchResults = await fetch(url);
+    const jsonData = await searchResults.json();
+    searchButton.disabled = false;
+    return jsonData.pokemon;
+   } catch {
+       searchButton.disabled = false;
+       return 'No results'
+   }
 }
 
 async function handleSearchResults() {
- if(pokemonList.childNode) pokemonList.childNodes.forEach(node => pokemonList.removeChild(node));
- console.log(searchBar.value)
- try {
-    const searchResults = await getPokemonType(searchBar.value).then(console.log);
- } catch {
-     console.log('hhhoho')
- }
- if (searchResults === 'No Results') {
+    if(document.querySelector('ul')) document.querySelector('ul').remove()
+    if(document.querySelector('p')) document.querySelector('p').remove()
+    const type = searchBar.value;
+    const searchResults = await getPokemonType(type)
+    console.log(searchResults)
+ if (searchResults === 'No results') {
+
      const noResults = document.createElement('p');
      noResults.textContent = 'Check your spelling and try again!'
      noResults.classList.add('no-results');
-     searchForm.append(noResults)
+     container.appendChild(noResults)
      
  } 
  else {
+     const unOrderedList = document.createElement('ul');
     for(let poke of searchResults) {
-        const li = document.createElement('li');
-        li.textContent(poke.name);
-        pokemonList.appendChild()
+        const listItem = document.createElement('li');
+        listItem.textContent = poke.pokemon.name;
+        listItem.classList.add('.list-item');
+        unOrderedList.appendChild(listItem)
     }
+    container.appendChild(unOrderedList)
+
  }
 
 }
